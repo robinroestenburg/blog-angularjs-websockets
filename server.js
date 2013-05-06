0b39f2f4681ff0bd924c5810b21a660fc9e98f72
Robin Roestenburg <robinroestenburg@gmail.com>
2013-05-06 20:49:53 +0200

    Initial commit.

---
+++ server.js
@@ -0,0 +1,25 @@
+var app    = require('express')(),
+    server = require('http').createServer(app),
+    io     = require('socket.io').listen(server);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+  function emitFibonacciNumber() {
+    setTimeout(function calculateFibonnaci(n_minus_1, n_minus_2) {
+      n = n_minus_1 + n_minus_2;
+
+      socket.emit('fibonacci', n);
+
+      n_minus_1 = n_minus_2;
+      n_minus_2 = n;
+
+      emitFibonacciNumber(n_minus_1, n_minus_2);
+    }, 1000);
+  };
+  emitFibonacciNumber(0, 1);
+
+});
